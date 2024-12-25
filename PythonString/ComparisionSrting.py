0a530e8516b134e65effc9e5da5da7de4de26e99
Masoom Raza <masoomansari0786@gmail.com>
2024-12-25 13:18:59 +0530

    Camprision code is commited

---
+++ PythonString/ComparisionSrting.py
@@ -0,0 +1,9 @@
+FirstString=input("Enter First String:")
+SecondString=input("Enter Second String:")
+
+if (FirstString==SecondString):
+    print("Strings are Identical")
+elif (FirstString<SecondString):
+    print("{} is Smaller than {} string".format(FirstString,SecondString))
+else:
+    print("{} is Bigger than {}".format(FirstString,SecondString))
