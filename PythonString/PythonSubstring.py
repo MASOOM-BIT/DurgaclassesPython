58d5a00911f49bd40b8c660f640994d678bc2819
Masoom Raza <masoomansari0786@gmail.com>
2024-12-25 13:01:26 +0530

    Substring code is commit

---
+++ PythonString/PythonSubstring.py
@@ -0,0 +1,7 @@
+MainInputSrting=input("Enter Main String: ")
+SubStringInput=input("Enter Substring to Searh in main string: ")
+
+if SubStringInput in MainInputSrting:
+    print("String Found")
+else:
+    print("Not Found")
