10f7b493e3077aa7b926607171041e7a1511714b
Masoom Raza <masoomansari0786@gmail.com>
2025-01-01 03:04:43 +0530

    Recursion

--- main.py
+++ main.py
@@ -4,13 +4,18 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def print_hi(n):
+    print('l.acquire()')
+    if n==0:
+        result=1
+    else:
+        result=n*print_hi(n-1)
+    print("l.release()")
+    return result
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print(print_hi(1))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
