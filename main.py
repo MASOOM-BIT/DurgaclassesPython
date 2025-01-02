315584da93f927076b366eee62e3cf9e636fd9ec
Masoom Raza <masoomansari0786@gmail.com>
2025-01-02 23:18:51 +0530

    Recursion

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def print_hi(n):
     print('l.acquire()')
     if n==0:
         result=1
+        print('Terminate Condition')
     else:
         result=n*print_hi(n-1)
+        print('Main Condition')
     print("l.release()")
     return result
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print(print_hi(1))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
