ddcd04f0f03bdc35d786236a2ea98415dd657953
Masoom Raza <masoomansari0786@gmail.com>
2025-01-06 22:48:02 +0530

    Dict Examples : Student RecordKeeping

--- PythonDictionary/DictExample4.py
+++ PythonDictionary/DictExample4.py
@@ -3,19 +3,22 @@ n=int(input("Enter No of Student : "))
 d={}
 for x in range(n):
     name = input("Name : ")
-    marks = input("Makrs : ")
-    d[name]=marks
+    marks = input("Marks out of 500: ")
+    roll_no = int(input("Roll No : "))
+    percentage =(int(marks)/500)*100
+    d[name]=(marks,roll_no,percentage)
 
 while True:
     name = input("Enter Name to fetch Marks: ")
-    marks = d.get(name,-1)
-    if marks==-1:
-        print('Student is not available :')
-    else:
-        print('Name :',name,'--> Marks:',marks)
+    student_info = d.get(name, -1)  # Get student info from dictionary
 
-    option = input(('Search Again [Y|N] : '))
+    if student_info == -1:
+        print('Student is not available')
+    else:
+        marks, roll_no,percentage = student_info  # Unpack the tuple
+        print('Name:', name, '--> | Marks:', marks, ' | Roll_No -->', roll_no,' | Percentage-->',percentage)
+
+    option = input('Search Again [Y|N] : ')
     if option == 'N' or option == 'n':
         break
 
-print('Thank You')
