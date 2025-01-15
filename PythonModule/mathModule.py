186d0aaeb2f707886cbfa9982968232de6132be0
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-01-16 00:29:19 +0530

    Add mathModule.py to demonstrate usage of various mathematical functions from the math module

---
+++ PythonModule/mathModule.py
@@ -0,0 +1,37 @@
+#Working with math module]
+#The math module is a standard module in Python and is always available. To use mathematical functions under this module, you have to import the module using import math.
+#The math module consists of the following functions:
+from math import *
+print("The value of pi is", pi) #Value of pi
+print("The value of e is", e) #Value of e
+print("The value of sin(90) is", sin(90)) #Value of sin(90)
+print("The value of cos(90) is", cos(90)) #Value of cos(90)
+print("The value of tan(90) is", tan(90)) #Value of tan(90)
+print("The value of log(10) is", log(10))
+print("The value of log10(10) is", log10(10))
+print("The value of exp(10) is", exp(10))
+print("The value of sqrt(100) is", sqrt(100))
+print("The value of ceil(10.1) is", ceil(10.1))
+print("The value of floor(10.9) is", floor(10.9))
+print("The value of fabs(-10) is", fabs(-10))
+print("The value of factorial(5) is", factorial(5))
+print("The value of fmod(10,3) is", fmod(10,3))
+print("The value of pow(10,2) is", pow(10,2))
+print("The value of modf(10.2) is", modf(10.2))
+print("The value of trunc(10.2) is", trunc(10.2))
+print("The value of degrees(10) is", degrees(10))
+print("The value of radians(10) is", radians(10))
+print("The value of hypot(10,10) is", hypot(10,10))
+print("The value of atan(10) is", atan(10))
+print("The value of atan2(10,10) is", atan2(10,10))
+print("The value of asin(1) is", asin(1))
+print("The value of acos(1) is", acos(1))
+print("The value of tanh(10) is", tanh(10))
+print("The value of sinh(10) is", sinh(10))
+print("The value of cosh(10) is", cosh(10))
+print("The value of asinh(10) is", asinh(10))
+print("The value of acosh(10) is", acosh(10))
+print("The value of erf(10) is", erf(10))
+print("The value of erfc(10) is", erfc(10))
+print("The value of gamma(10) is", gamma(10))
+print("The value of lgamma(10) is", lgamma(10))
