8ddff36e18c3c0a20a13ffa56e9a3fcdf162aa8a
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-01-23 01:40:00 +0530

    Add IoggingImplementation.py with documentation for logging levels and usage examples

---
+++ PythonLogs/IoggingImplementation.py
@@ -0,0 +1,25 @@
+'''
+1. Name of the file
+2.Level of message to store
+
+basicConfig() function of logging module
+
+Syntax : 
+
+logging.basicConfig(filename='log.txt' , level=logging.WARNING)
+
+level = logging.WARNING ->provide log warning and Higher
+
+'''
+
+'''
+# How to Write in Log file
+
+>logging.debug(message)
+>logging.info(message)
+>logging.warning(message)
+>logging.error(message)
+>logging.critical(message)
+
+'''
+
