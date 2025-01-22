733980f5438adcfd42605e69dfc5c400d023ad93
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-01-23 01:39:55 +0530

    Add LoggingModule.py to implement a logging system with severity levels and debugging capabilities

---
+++ PythonLogs/LoggingModule.py
@@ -0,0 +1,28 @@
+'''
+> Keep records of all the activity
+> use logfile
+> Track complete information
+> If something goes wrong we can identify the person responsible
+> Use for Debugging Purposes
+> We can provide data like number of request
+
+Logging Module (Inbuilt Python Libarary):
+
+Logging Levels : We can provide log base on severity
+
+LEVELS:
+
+1.CRITICAL : code - 50 : Represent very serious problem that need a high attentions
+
+2.ERROR :code - 40 : Represent very serious Problem but attention level is bit low
+
+3.WARNING : Code - 30 : Alert to the programmer
+
+4.INFO : code - 20 : Represent information to the Programmer
+
+5.DEBUG : code - 10 : Represent information with debugging msg
+
+6.NOTSET : code - 0 : Represent level is not set
+
+NOTE : By default only critical , Error , warning only display in logging file
+'''
