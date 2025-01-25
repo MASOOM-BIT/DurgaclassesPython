d201ec4ab32b3247df68407ad46f3363e811e93b
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-01-26 00:44:27 +0530

    Add Writing_data_in_file.py to demonstrate file writing techniques in Python

---
+++ PythonFileHandling/Writing_data_in_file.py
@@ -0,0 +1,20 @@
+'''
+> Use the open() function to create or open a file. Always specify the mode you want to use.
+
+> The write() method writes a string to the file, while writelines() can be used to write a list of strings.
+> f.write(str)
+> f.writelines(list_of_lines)
+
+'''
+f = open('abcd.txt','w')
+f.write('durga\n')
+f.write('Software\n')
+f.write('Solution\n')
+print("Data Written")
+
+f = open('abcde.txt','w')
+list = ['sunny\n','bunny\n','chinny\n']
+f.writelines(list)
+print("Data written")
+f.close()
+
