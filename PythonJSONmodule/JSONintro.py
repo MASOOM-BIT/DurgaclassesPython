d7a6ba56b1be24867c7b4222bb07b989cf2443f4
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-01-30 03:12:45 +0530

    Add JSONintro.py to introduce JSON data format and its basic structures

---
+++ PythonJSONmodule/JSONintro.py
@@ -0,0 +1,14 @@
+# JSON, which stands for JavaScript Object Notation, is a popular data format for online data exchange. JSON is the best format for organizing data between a client and a server. The programming language JavaScript is comparable to this language's syntax. JSON's primary goal is data transmission between the client and the web server. It is the most efficient method of exchanging data and is simple to master.
+'''
+>>> JSON primarily supports the following six forms of data:
+
+> String
+> Number
+> Boolean
+> Null
+> Object
+> Array
+'''
+import json
+print(dir(json))
+#['JSONDecodeError', 'JSONDecoder', 'JSONEncoder', '__all__', '__author__', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__path__', '__spec__', '__version__', '_default_decoder', '_default_encoder', 'codecs', 'decoder', 'detect_encoding', 'dump', 'dumps', 'encoder', 'load', 'loads', 'scanner']
