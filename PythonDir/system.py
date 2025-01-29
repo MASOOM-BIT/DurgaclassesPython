f8c7c2c1d0c4e3f5d627c925108dde3e08668476
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-01-30 03:12:10 +0530

    Add system.py to demonstrate usage of the sys module for executing commands and managing the environment

---
+++ PythonDir/system.py
@@ -0,0 +1,15 @@
+'''
+> The sys module is a powerful tool in Python that allows you to interact with the interpreter and manage various aspects of your program's execution environment. Whether you're working with command-line arguments, handling input/output streams, or retrieving system information, understanding how to use the sys module can greatly enhance your scripting capabilities.
+
+> The sys module in Python provides access to some variables and functions that interact with the Python interpreter. It is a standard library module, meaning it comes pre-installed with Python, and you can use it without any additional installation.
+'''
+#os.system('execute all cmd commands')
+
+import os
+os.system('dir *.py')
+
+#running one python file from another python file
+os.system('python PythonDir/OsModule.py')
+
+#to open notepad
+os.system('notepad')
