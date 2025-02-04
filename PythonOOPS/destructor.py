09f0f5d0ebc2ab94b72ffba493893df71ac95dfa
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-02-05 03:11:50 +0530

    Add destructor.py to demonstrate the use of destructors in Python and object cleanup

---
+++ PythonOOPS/destructor.py
@@ -0,0 +1,23 @@
+'''
+>>> Destructors:
+> destructor is called by GC to perform destruction of object
+syntax : __del__()
+> 'gc' always call destructor to perform cleanup code before destroying object
+> to fullfill last wish 'gc' will call 
+__del__(self):
+    #cleanup code
+
+> the job of destructor is to perform cleanup cactivities just before destroying an object
+> destroctor never destroy object 'gc' do
+'''
+import time
+class Test:
+    def __init__(self):
+        print('Objects intilixation')
+    def __del__(self):
+        print("Fullfulling last wish/Clean Activity") #execte auto as soon as t1=Nonw will execute
+
+t1=Test()
+t1=None #Now eligible for gc
+time.sleep(5)
+print('End of Application')
