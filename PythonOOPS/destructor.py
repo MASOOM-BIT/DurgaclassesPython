35c6511e6139e3239fade73d78573306d0a54a85
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-02-06 03:13:15 +0530

    Add examples to destructor.py to illustrate destructor functionality in Python

--- PythonOOPS/destructor.py
+++ PythonOOPS/destructor.py
@@ -10,6 +10,7 @@ __del__(self):
 > the job of destructor is to perform cleanup cactivities just before destroying an object
 > destroctor never destroy object 'gc' do
 '''
+# Example 1
 import time
 class Test:
     def __init__(self):
@@ -21,3 +22,15 @@ t1=Test()
 t1=None #Now eligible for gc
 time.sleep(5)
 print('End of Application')
+
+# Example 2
+class Test:
+    def __init__(self):
+        print('Constructor Executed')
+    def __del__(self):
+        print('Deconstructor Executed')
+
+t1=Test()
+del t1
+time.sleep(5)
+print('End')
