a313be576771885aa72ea8017c3acff389b931b7
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-02-12 03:13:23 +0530

    Add example1.py to demonstrate basic threading with a child thread

---
+++ MultiThreading/example1.py
@@ -0,0 +1,9 @@
+from threading import *
+def display():
+    for i in range(10):
+        print('Child thread or thread-01')
+    
+t=Thread(target=display)
+t.start()
+for i in range(10):
+    print('Main Thread')
