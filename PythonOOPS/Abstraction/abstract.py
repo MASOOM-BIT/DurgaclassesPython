b516e71ba7a21f77531e00db490e66537f9e971e
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-02-25 02:37:43 +0530

    Add database connection classes for Oracle, MySQL, and PostgreSQL

--- PythonOOPS/Abstraction/abstract.py
+++ PythonOOPS/Abstraction/abstract.py
@@ -5,4 +5,29 @@ class DBInterface(ABC):
         pass
     @abstractmethod
     def disconnect(self):
-        pass 
+        pass
+
+class Oracle(DBInterface):
+    def connect(self):
+        print('Connecting to Oracle DB')
+    def disconnect(self):
+        print('Disconnecting from Oracle DB')
+
+class Mysql(DBInterface):
+    def connect(self):
+        print('Connecting to Mysql DB')
+    def disconnect(self):
+        print('Disconnecting from Mysql DB')
+
+class PostgreSQL(DBInterface):
+    def connect(self):
+        print('Connecting to PostgreSQL DB')
+    def disconnect(self):
+        print('Disconnecting from PostgreSQL DB')
+
+
+dbname=input('Enter Db Name: ')
+classname=globals()[dbname] #inbuilt function 'globals' converts string into class and return the class name
+x=classname()
+x.connect()
+x.disconnect()
