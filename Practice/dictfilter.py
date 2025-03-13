dd675c65626bbafdf7b6d646e86dcc7ba9fb6540
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-03-14 03:17:14 +0530

    practice

---
+++ Practice/dictfilter.py
@@ -0,0 +1,7 @@
+# Original Dictionary:
+# {'Cierra Vega': (6.2, 70), 'Alden Cantrell': (5.9, 65), 'Kierra Gentry': (6.0, 68), 'Pierre Cox': (5.8, 66)}
+# Height > 6ft and Weight> 70kg:
+# {'Cierra Vega': (6.2, 70)}
+
+original_dict = {'Cierra Vega': (6.2, 70), 'Alden Cantrell': (5.9, 65), 'Kierra Gentry': (6.0, 68), 'Pierre Cox': (5.8, 66)}
+
