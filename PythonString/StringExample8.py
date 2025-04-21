4eca246e3e9a438627df9ec8265108739e966e71
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-04-22 01:39:41 +0530

    String odd seq Example

---
+++ PythonString/StringExample8.py
@@ -0,0 +1,4 @@
+# Write a Python program to remove characters that have odd index values in a given string.
+sample_string = "The quick brown fox jumps over the lazy dog."
+
+print(sample_string[::2])
